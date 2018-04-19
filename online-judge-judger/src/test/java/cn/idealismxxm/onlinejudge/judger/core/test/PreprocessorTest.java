98bbf9abf3f3a947e3d4acbd663dc6c2eb2a118c
idealism-xxm <biaochekuangmo@yeah.net>
2018-04-19 16:34:29 +0000

    删除没有用到包

--- online-judge-judger/src/test/java/cn/idealismxxm/onlinejudge/judger/core/test/PreprocessorTest.java
+++ online-judge-judger/src/test/java/cn/idealismxxm/onlinejudge/judger/core/test/PreprocessorTest.java
@@ -1,35 +1,17 @@
 package cn.idealismxxm.onlinejudge.judger.core.test;
 
-import cn.idealismxxm.onlinejudge.domain.entity.Description;
-import cn.idealismxxm.onlinejudge.domain.entity.Problem;
 import cn.idealismxxm.onlinejudge.domain.entity.Submission;
-import cn.idealismxxm.onlinejudge.domain.entity.TestCase;
-import cn.idealismxxm.onlinejudge.domain.enums.OnlineJudgeEnum;
-import cn.idealismxxm.onlinejudge.domain.enums.PublicStatusEnum;
-import cn.idealismxxm.onlinejudge.domain.util.JsonUtil;
 import cn.idealismxxm.onlinejudge.judger.core.Preprocessor;
 import cn.idealismxxm.onlinejudge.service.SubmissionService;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * Preprocessor 测试类
